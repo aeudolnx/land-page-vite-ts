7f27ad677373d98a2d09e155119e3ee51cfadecb
Aeudo Pereira <aeudolinux@gmail.com>
2024-07-23 22:22:39 -0300

    consertando algo que estava errado

--- src/pages/header/index.tsx
+++ src/pages/header/index.tsx
@@ -4,7 +4,7 @@ import styles from './styles.module.scss';
 export function Header() {
   return (
     <main className={styles.Container}>
-      <H1 text="Vai se fuder" size="xhuge" />
+      <H1 text="Hello Wrld 999!" size="xhuge" />
     </main>
   );
 }
